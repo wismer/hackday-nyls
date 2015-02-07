5ec627af998fb2e8d8d5e52f534693ae895aefea
Matt Long <matthewhl@gmail.com>
2015-02-07 16:55:34 -0500

    omg

--- unpacker.py
+++ unpacker.py
@@ -3,6 +3,12 @@ import cPickle as pickle
 
 
 def unpack(filename="accidents.p"):
-	data=pickle.load(open(filename,"rb"))
-	jsondata=json.dumps(data)
-	return jsondata
+  data=pickle.load(open(filename,"rb"))
+  jsonfile = open("data.json", "wb")
+  jsondata=json.dumps(data)
+  jsonfile.write(jsondata)
+
+  return jsondata
+
+
+unpack()
