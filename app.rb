fc59a452c6f2dd0fa595462853e7045090bb18be
Matt Long <matthewhl@gmail.com>
2015-02-12 15:11:22 -0500

    stuff

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class BikeApp < Sinatra::Base; end
 class BikeApp
   get "/" do
     data = File.read("data.json")
-    
-    erb :index, locals: { data: "<script>var data = #{data}</script>" }
+    keys = JSON.parse(data).keys
+    erb :index, locals: { data: "<script>var data = #{data}</script>", dates: keys }
   end
 
 
